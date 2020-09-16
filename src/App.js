fe45be8be6352a327f234425045c25f963f9d85c
teamzero-aaq <abdulquadir828@hmail.com>
2020-09-16 19:32:06 +0530

    Inital Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>This is Search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
